733a35cc8221b5f5c552bd1bc3823867d3043929
Рузиль <ruzil.gil1@gmail.com>
2025-03-12 00:13:10 +0500

    Домашнее задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир!!!");
-        System.out.println("Hello World!!!");
+        System.out.println("Задача 1");
+        var dog=8.0;
+        System.out.println(dog);
+        var kat=3.6;
+        System.out.println(kat);
+        var paper=763789;
+        System.out.println(paper);
+        System.out.println("Задача 2");
+        System.out.println(dog+4);
+        System.out.println(kat+4);
+        System.out.println(paper+4);
+        System.out.println("Задача 3");
+        System.out.println(dog+3.5);
+        System.out.println(kat-1.6);
+        System.out.println(paper-7639);
+        System.out.println("Задача 4");
+        var friend=19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        System.out.println("Задача 5");
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        System.out.println("Задача 6");
+        var МассаОдногоБоксера= 78.2;
+        var МассаВторогоБоксера= 82.7;
+        var ОбщаяМассаДвухБойцов=МассаОдногоБоксера+МассаВторогоБоксера;
+        System.out.println("Общая масса двух бойцов " + ОбщаяМассаДвухБойцов + " кг");
+        var РазницуМеждуМассамиБойцов=МассаВторогоБоксера-МассаОдногоБоксера;
+        System.out.println("Разницу между массами бойцов " + РазницуМеждуМассамиБойцов + " кг");
+        System.out.println("Задача 7");
+        var ОстатокОтДеления=МассаВторогоБоксера%МассаОдногоБоксера;
+        System.out.println("Остаток от деления " + ОстатокОтДеления);
+        System.out.println("Задача 8");
+        var прервое=640/8;
+        System.out.println("1) Всего работников в компании- " + прервое + " человек");
+        var второе=80+94;
+        var третий=640/второе;
+        System.out.println("2) Если в компании работает " + второе + " человек, то всего " + третий + " часов работы может быть поделено между сотрудниками ");
+
+
+
+
+
+
+
+
+
     }
 }
