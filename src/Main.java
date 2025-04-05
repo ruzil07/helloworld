eedb942d1c3d239e3563fa3d6df5800e9779440a
Рузиль <ruzil.gil1@gmail.com>
2025-04-06 03:25:22 +0500

    Домашнее задание №6 . 8 8 задание не понял как это выражение вывел 1896 1975 и 2054

--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,98 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("Задание 1");
-        int age = 12;
-        if (age >= 18) {
-            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
-        } else {
-            System.out.println("Если возраст человека равен " + age + ",он не достиг совершеннолетия," +
-                    " нужно немного подождать");
+        int desiredSun = 2_459_000;
+        int piggyBank = 0;
+        int savings = 15000;
+        double persent = 1D / 100;
+        int month = 0;
+        while (piggyBank < desiredSun) {
+            piggyBank += savings;
+            piggyBank = (int) (piggyBank * (1 + persent));
+            month++;
+            System.out.println("Месяц " + month + ". сумма накоплений равна " + piggyBank + " рублей");
+        }
+
+        System.out.println();
+        System.out.print("Задание 2");
+        System.out.println();
+        int start = 1;
+        while (start <= 10) {
+            System.out.print(start + " ");
+            start = start + 1;
         }
         System.out.println();
-        System.out.println("Задание 2");
-        int temperature = 6;
-        if (temperature < 5) {
-            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку ");
-        } else {
-            System.out.println("На улице " + temperature + " градусов, можно идти без шапки ");
+        int start1 = 10;
+        for (; start1 > 0; start1--) {
+            System.out.print(start1 + " ");
         }
         System.out.println();
         System.out.println("Задание 3");
-        int speed = 65;
-        if (speed >= 60) {
-            System.out.println("Если скорость " + speed + "," + " то придется заплатить штраф.");
-        } else {
-            System.out.println("Если скорость " + speed + "," + " можно ездить спокойно.");
+        int population = 12_000_000;
+        int fertility = 17;
+        int mortality = 8;
+        int currentYear = 2024;
+        for (int year = currentYear; year < currentYear + 10; year++) {
+            population += population * fertility / 1000 - population * mortality / 1000;
+            System.out.println("Год " + year + ", чесленность населения составляет " + population);
         }
         System.out.println();
         System.out.println("Задание 4");
-        int age1 = 5;
-        if (age1 >= 2 && age1 <= 6) {
-            System.out.println("Если возраст человека равен " + age1 + "," + " то ему нужно ходить в детский сад.");
-        } else if (age1 >= 7 && age1 <= 17) {
-            System.out.println("Если возраст человека равен " + age1 + "," + " то ему нужно ходить в школу.");
-        } else if (age1 >= 18 && age1 <= 24) {
-            System.out.println("Если возраст человека равен " + age1 + "," + " то его место в университете.");
-        } else if (age1 >= 25) {
-            System.out.println("Если возраст человека равен " + age1 + "," + " то ему пора ходить на работу.");
+        persent = 7D / 100;
+        desiredSun = 12000000;
+        piggyBank = savings;
+        month = 0;
+        while (piggyBank < desiredSun) {
+            piggyBank = (int) (piggyBank * (1 + persent));
+            month++;
+            System.out.println("Месяц " + month + ". сумма накоплений равна " + piggyBank + " рублей");
         }
         System.out.println();
         System.out.println("Задание 5");
-        int age5 = 2;
-        if (age5 < 5) {
-            System.out.println("Если возраст ребенка равен " + age5 + "," + " то ему нельзя кататься на аттракционе");
-        }
-        if (age5 >= 5 && age5 < 14) {
-            System.out.println("Если возраст ребенка равен " + age5 + "," + " то ему можно кататься на аттракционе" +
-                    " в сопровождении");
-        }
-        if (age5 >= 14) {
-            System.out.println("Если возраст ребенка равен " + age5 + "," + " то он может кататься без " +
-                    "сопровождения взрослого");
+        persent = 7D / 100;
+        desiredSun = 12000000;
+        piggyBank = savings;
+        month = 0;
+        while (piggyBank < desiredSun) {
+            piggyBank = (int) (piggyBank * (1 + persent));
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ". сумма накоплений равна " + piggyBank + " рублей");
+            }
         }
         System.out.println();
         System.out.println("Задание 6");
-        int human = 102;
-        if (human <= 60) {
-            System.out.println("Вагоне есть сидщее места.");
-        } else if (human > 60 && human <= 102) {
-            System.out.println("Вагоне есть стоящее места.");
-        } else if (human > 102) {
-            System.out.println("Вагон уже полностью забит.");
+        persent = 7D / 100;
+        desiredSun = 12000000;
+        piggyBank = savings;
+        month = 0;
+        int months = 12 * 9;
+        while (month < months) {
+            piggyBank = (int) (piggyBank * (1 + persent));
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ". сумма накоплений равна " + piggyBank + " рублей");
+            }
         }
         System.out.println();
         System.out.println("Задание 7");
-        int one = 30;
-        int two = 22;
-        int three = 10;
-        if (one > two && one > three) {
-            System.out.println(one);
-        } else if (two > one && two > three) {
-            System.out.println(two);
-        } else if (three > one && three > two) {
-            System.out.println(three);
+        int firstFridey=4;
+        for ( int day= firstFridey; day<=31; day+=7) {
+            System.out.println("Сегодня пятница, " + day+ " -ечисло. Необходимо подготовить отчет");
+        }
+        System.out.println();
+        System.out.println("Задание 8");
+        // Я тне понимаю как это рондомный набор выводит 1896 1975 и 2054. Я смотрел видео как он обьяснял это но не
+        // понял . Тупо переписал и все
+        int period=79;
+        int startSeeing=0;
+        start = currentYear - 200;
+        int end = currentYear+100;
+        for (int year = startSeeing; year < end; year+= period ) {
+            if (year> start){
+                System.out.println(year);
+            }
         }
     }
 }
+
