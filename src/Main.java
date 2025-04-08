0477033281f728a488f4d7975d5ab8b999d81501
Рузиль <ruzil.gil1@gmail.com>
2025-04-08 22:32:01 +0500

    Домашнее задание №7.

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,80 @@ public class Main {
         System.out.println("Задание 1");
         // как будь то китаец написпал условие этого задания. Не по русский написано. очень сложно понять какой
         // ответ хотите увидеть.
-        float[] floatArr = new float[3];
-        floatArr[0] = 1.57f;
-        floatArr[1] = 7.654f;
-        floatArr[2] = 9.986f;
+        int[] intArr = new int[3];
+        intArr[0] = 1;
+        intArr[1] = 2;
+        intArr[2] = 3;
+        float[] floatArr = {1.57f, 7.654f, 9.986f};
+        int[] intArr1 = new int[9];
+        intArr1[0] = 1;
+        intArr1[1] = 2;
+        intArr1[2] = 0;
+        intArr1[3] = 1;
+        intArr1[4] = 5;
+        intArr1[5] = 1;
+        intArr1[6] = 9;
+        intArr1[7] = 1;
+        intArr1[8] = 10;
 
         System.out.println();
         System.out.println("Задание 2");
         //Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую. В конце строки
         // запятую ставить не надо.- Какого массива ? Откуда взять какой массив? Я взял с первого задания.
-        System.out.println(Arrays.toString(floatArr));
+        for (int i = 0; i < intArr.length; i++) {
+            if (i == intArr.length - 1) {
+                System.out.println(intArr[i]);
+                break;
+            }
+            System.out.print(intArr[i] + ", ");
+        }
+        for (int i = 0; i < floatArr.length; i++) {
+            if (i == floatArr.length - 1) {
+                System.out.println(floatArr[i]);
+                break;
+            }
+            System.out.print(floatArr[i] + ", ");
+        }
+        for (int i = 0; i < intArr1.length; i++) {
+            if (i == intArr1.length - 1) {
+                System.out.println(intArr1[i]);
+                break;
+            }
+            System.out.print(intArr1[i] + ", ");
+        }
 
         System.out.println();
         System.out.println("Задание 3");
-        int[] numbers = {1, 2, 3};
-        System.out.print(numbers[2] + ", ");
-        System.out.print(numbers[1] + ", ");
-        System.out.println(numbers[0]);
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            System.out.print(floatArr[i]);
-            if (i != 0) {
-                System.out.print(", ");
+        for (int i = intArr.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(intArr[i]);
+                break;
             }
-
+            System.out.print(intArr[i] + ", ");
         }
+        for (int i = floatArr.length-1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(floatArr[i]);
+                break;
+            }
+            System.out.print(floatArr[i] + ", ");
+        }
+        for (int i = intArr1.length-1; i >=0; i--) {
+            if (i == 0) {
+                System.out.println(intArr1[i]);
+                break;
+            }
+            System.out.print(intArr1[i] + ", ");
+        }
+
         System.out.println();
         System.out.println("Задание 4");
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                numbers[i] += 1;
+        for (int i = 0; i < intArr.length; i++) {
+            if (intArr[i] % 2 != 0) {
+                intArr[i] += 1;
             }
         }
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(intArr));
     }
 }
 
